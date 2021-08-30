bf215aa65401fd63d54408de2a50fe7c31fa9c72
hamabe <hamabe@neopa.jp>
2021-08-30 20:08:37 +0900

    PORT指定

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3030);
+  await app.listen(process.env.PORT || 3030);
   console.log("start")
 }
 bootstrap();
