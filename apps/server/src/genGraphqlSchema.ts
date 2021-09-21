4de79b50896ecc0fcab474cfbca889755a87caf0
hamabe <hamabe@neopa.jp>
2021-09-21 22:31:37 +0900

    graphqlスキーマの型ファイル生成コマンド追加

---
+++ apps/server/src/genGraphqlSchema.ts
@@ -0,0 +1,16 @@
+import {GraphQLSchemaBuilderModule, GraphQLSchemaFactory} from '@nestjs/graphql';
+import {printSchema} from 'graphql';
+import {NestFactory} from '@nestjs/core';
+import {UserResolver} from './presentation/controllers/user/user.resolver';
+async function generateSchema() {
+    const app = await NestFactory.create(GraphQLSchemaBuilderModule);
+    await app.init();
+
+    const gqlSchemaFactory = app.get(GraphQLSchemaFactory);
+    const schema = await gqlSchemaFactory.create([
+        UserResolver
+    ]);
+    console.log(printSchema(schema));
+}
+
+generateSchema();
