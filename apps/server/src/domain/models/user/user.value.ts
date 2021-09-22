a85ddec80a698d60a126a8c5fbf4aea0ed1b9e3c
hamabe <hamabe@neopa.jp>
2021-09-22 11:36:29 +0900

    [server] userModel: 値オブジェクトの値チェックはコンストラクタ内に書く

--- apps/server/src/domain/models/user/user.value.ts
+++ apps/server/src/domain/models/user/user.value.ts
@@ -2,22 +2,20 @@ import {IncompatibleValueError} from '../../../types/error.types';
 import {RegexConst} from '../../../const/regex.const';
 import * as crypto from 'crypto';
 import {AbstractValueObject} from '../../../abstract/abstract-value-object';
+import {StringUtil} from '../../../utils/String.util';
 
 /**
  * ユーザーの名前の値オブジェクト
  */
 export class UserName extends AbstractValueObject<string> {
+    private MAX_LENGTH = 20;
+    private MIN_LENGTH = 1;
     constructor(value: string) {
         super(value);
-        this.checkLength(value);
-    }
-
-    checkLength(value: string) {
-        if(value.length <= 1 || value.length >= 20) {
-            throw new IncompatibleValueError("ユーザーの名前は1文字以上、20文字以下で設定してください。")
+        if(!StringUtil.between(this.MIN_LENGTH, this.MAX_LENGTH, value)) {
+            throw new IncompatibleValueError(`ユーザーの名前は${this.MIN_LENGTH}文字以上、${this.MAX_LENGTH}文字以下で設定してください。`)
         }
     }
-
 }
 
 /**
@@ -26,10 +24,6 @@ export class UserName extends AbstractValueObject<string> {
 export class UserMail extends AbstractValueObject<string> {
     constructor(value: string) {
         super(value);
-        this.checkFormat(value)
-    }
-
-    checkFormat(value: string) {
         const regex = new RegExp(RegexConst.EMAIL)
         if(!regex.test(value)){
             throw new IncompatibleValueError("正しいメールアドレスの形式を設定してください。")
@@ -44,22 +38,24 @@ export class UserHashedPass extends AbstractValueObject<string> {
     private MAX_LENGTH = 16;
     private MIN_LENGTH = 8;
     constructor(value: string) {
+
         super(UserHashedPass.hashed(value));
-        this.checkLength(value);
-        this.checkFormat(value);
-    }
-    static hashed(value: string): string {
-        return crypto.createHash('sha512').update(value).digest('hex');
-    }
-    checkLength(value: string) {
+
         if(value.length <= this.MIN_LENGTH || value.length >= this.MAX_LENGTH) {
             throw new IncompatibleValueError(`ユーザーのパスワードは${this.MIN_LENGTH}文字以上、${this.MAX_LENGTH}文字以下で設定してください。`)
         }
-    }
-    checkFormat(value: string) {
+
         const regex = new RegExp(RegexConst.HALF_WIDTH_ALPHANUMERIC)
         if(!regex.test(value)){
             throw new IncompatibleValueError("ユーザーのパスワードは英数字を設定してください。")
         }
+    }
+
+    /**
+     * 文字列をハッシュ化する
+     * @param value
+     */
+    static hashed(value: string): string {
+        return crypto.createHash('sha512').update(value).digest('hex');
     }
 }
---
+++ apps/server/src/utils/String.util.ts
@@ -0,0 +1,11 @@
+export class StringUtil {
+    /**
+     * 与えられた文字列valueが端値を含む範囲の文字数にあるか
+     * @param min
+     * @param max
+     * @param value
+     */
+    static between(min: number, max: number, value: string) {
+        return !!(min <= value.length && value.length <= max);
+    }
+}
