c572b03cb1876881571a787b57be3354981b2578
hamabe <hamabe@neopa.jp>
2021-09-22 16:07:20 +0900

    [server] dto: バリデーションを追加

--- apps/server/src/presentation/controllers/user/user.dto.ts
+++ apps/server/src/presentation/controllers/user/user.dto.ts
@@ -1,5 +1,7 @@
 import {Field, InputType, ObjectType} from '@nestjs/graphql';
 import {UserModel} from '../../../domain/models/user/user.model';
+import {Matches, MaxLength, MinLength} from 'class-validator';
+import {RegexConst} from '../../../const/regex.const';
 
 @InputType()
 export class SignupUserRequest {
@@ -10,6 +12,9 @@ export class SignupUserRequest {
     readonly mail: string;
 
     @Field()
+    @MinLength(8, {message: 'ユーザーのパスワードは8文字以上に設定してください。'})
+    @MaxLength(16, {message: 'ユーザーのパスワードは16文字以下に設定してください。'})
+    @Matches(RegexConst.HALF_WIDTH_ALPHANUMERIC, {message: 'ユーザーのパスワードは英数字を設定してください。'})
     readonly password: string;
 }
 
