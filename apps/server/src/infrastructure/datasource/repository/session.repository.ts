fbe9a8976b038a78abd07144841f1441487afe04
hamabe <hamabe@neopa.jp>
2021-09-23 15:02:54 +0900

    [server] repo: Diデコレータ付与

--- apps/server/src/infrastructure/datasource/repository/session.repository.ts
+++ apps/server/src/infrastructure/datasource/repository/session.repository.ts
@@ -1,14 +1,17 @@
 import {ISessionRepository} from '../../../domain/models/session/session.repository.interface';
 import {UserModel} from '../../../domain/models/user/user.model';
 import {SessionModel} from '../../../domain/models/session/session.model';
-import {ServiceUnavailableException} from '@nestjs/common';
+import {Injectable, ServiceUnavailableException} from '@nestjs/common';
 import {MyUtil} from '../../../utils/my.util';
 
+@Injectable()
 export class SessionRepository extends ISessionRepository {
     create(user: UserModel): Promise<SessionModel> {
         throw new ServiceUnavailableException();
     }
 }
+
+@Injectable()
 export class SessionRepositoryMock extends ISessionRepository {
     create(user: UserModel): Promise<SessionModel> {
         return new Promise((resolve)=>{
