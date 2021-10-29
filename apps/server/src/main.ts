a2fd745d71224856e378823dfd2e73625e20f979
hamabe <hamakyo711@gmail.com>
2021-10-29 22:26:35 +0900

    [server] frontのstaticファイルを提供

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,9 +1,22 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {ValidationPipe} from '@nestjs/common';
-
+import {NestExpressApplication} from '@nestjs/platform-express';
+import {join} from 'path';
+/**
+ * return static files path
+ */
+const _staticFilesPathGen = (): string => {
+    return ~__dirname.indexOf('/server/dist/src')
+        ? join(__dirname, '..', '..', '..', 'front', 'dist')
+        : join(__dirname, '..', '..', 'front', 'dist');
+};
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    const staticFilesPath: string = _staticFilesPathGen();
+    console.log(`staticFilesPath: ${staticFilesPath}`);
+    app.useStaticAssets(staticFilesPath);
+
     app.useGlobalPipes(new ValidationPipe({}));
     await app.listen(process.env.PORT || 3030);
     console.log('start');
