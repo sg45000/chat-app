eaec4fe48c9dcc918d74cf5406f07dfa2c5ac9ed
hamabe <hamakyo711@gmail.com>
2021-10-31 09:54:50 +0900

    [front] room新規作成ページ

---
+++ apps/front/pages/room/new.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-text-field
+          v-model="name"
+          :rules="rules.name"
+          :counter="25"
+          label="トークルーム名"
+        >
+        </v-text-field>
+      </v-row>
+    </v-container>
+    <v-button>
+    </v-button>
+  </v-form>
+</template>
+
+<script lang="ts">
+import CustomVue from '~/custom';
+import {roomRules} from '~/rules/room.rules';
+interface Data {
+  name: string;
+  rules: {
+    name: ((v:string) => string | boolean)[],
+  }
+}
+export default CustomVue.extend({
+  name: 'room/new',
+  data(): Data  {
+    return {
+      name : '',
+      rules: {
+        name: [
+          roomRules.validNameLength,
+        ]
+      }
+    };
+  },
+  async created() {
+  },
+  methods: {
+  }
+});
+</script>
+
+<style scoped>
+
+</style>
